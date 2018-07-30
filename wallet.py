357388d204e40d4948a050d0e60007eee3588b35
Ecko <bboy_19ecko89@hotmail.com>
2018-07-30 15:35:26 +0200

    Add files via upload

---
+++ wallet.py
@@ -0,0 +1,23 @@
+"""Define class wallet, initiate with values
+print values given, do simple maths on values"""
+
+class wallet:
+    def __init__(self,money,cards,receipts,name):
+        self.money,self.cards,self.receipts,self.name=money,cards,receipts,name
+
+    def hoeveel(self):
+        print(f'There are  {self.money+self.cards+self.receipts} items in {self.name}')
+
+    def vakjes(self,cards_vakjes):
+        print(f'There are {cards_vakjes} sleeves in {self.name}',end='')
+        print(f', of which {cards_vakjes-self.cards} unused')
+
+potmoni=wallet(3,3,2,'potmoni') #2 objects of class wallet
+portomonnee=wallet(10,8,5,'portomonnee')
+cardholder=wallet(0,4,0,'cardholder')
+
+obj_wallet=[potmoni,portomonnee,cardholder]
+
+for i in obj_wallet:
+    i.hoeveel()
+    i.vakjes(20)
