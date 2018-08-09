5709911b7dbf8e37a7043a083a9d2719157c77ba
Ecko <bboy_19ecko89@hotmail.com>
2018-08-09 21:26:31 +0200

    using list comprehension to create multiple objects

--- oranges.py
+++ oranges.py
@@ -3,8 +3,8 @@ More OOP practice'''
 import datetime, random
 
 class Oranges:
-    all_oranges=[]
     'Nothing rhymes with orange'
+    all_oranges=[]
     def __init__(self):
         self.id=random.randrange(5000000,9999999)
         self.day_picked=datetime.datetime.now()
@@ -14,8 +14,6 @@ class Oranges:
     def baskid(self,basketid):
         self.bask=basketid
         
-        
-    
     def identify(self):
         print(f'orangeid={self.id}\ntime picked={self.day_picked}\nbasket={self.bask}')
 
@@ -32,12 +30,15 @@ class Baskets:
 
     def identify(self):
         print(f'id={self.id}\nday created={self.day_created}\norange amount={len(self.orange_id)}\noranges in basket={self.orange_id}')
+        
 
-#initialize an orange and basket
-o=Oranges()
-b=Baskets()
+#initialize oranges and baskets
+O=[Oranges() for i in range(10)] #new way of creating multiple obj
+B=[Baskets() for i in range(2)]
 
-o.baskid(b.add_orange(o.id))
-b.identify()
-print('\n')
-o.identify()
+for i in O:
+    j=random.choice(B)
+    i.baskid(j.add_orange(i.id))
+
+for a in B:
+    print(a.identify())
