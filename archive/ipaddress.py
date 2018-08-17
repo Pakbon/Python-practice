ecb6bf0e77114fa9c9e0b83e3b8f7fdc0d3f0f5c
Ecko <bboy_19ecko89@hotmail.com>
2018-08-17 17:49:14 +0200

    rewrite script
    Making it somewhat comprehensible

--- archive/ipaddress.py
+++ archive/ipaddress.py
@@ -1,29 +1,36 @@
-import os
-import re
+import os, re, subprocess, datetime
 
-def bestand(file, n, text):
-	if n == 'openen':
-		inputfile= open(file)
-		findIP(file)
-		inputfile.close()
-	else:
-		logwrite=open(logfile,'a')
-		logwrite.write(text)
-		logwrite.write('\n')
-		logwrite.close()
+dt=datetime.date.today()
+dt=dt.strftime("%Y-%m-%d")
+curdir=os.getcwd()
+logfile='{}/{}.log'.format(curdir, dt)
+grepfile='{}/{}.grep'.format(curdir, dt)
+uinput='/var/log/auth.log'
+nmap_args=['/usr/bin/nmap', '-iL', logfile, '-n', '-F', '-sV','--version-light', '-oG', grepfile]
 
-def findIP(textfile):
-	iplist=open(textfile)
-	iplistread=iplist.read()
-	reg= re.compile(r'\d+\.\d+\.\d+\.\d+')
-	results=reg.findall(iplistread)
-	results.sort()
-	for i in results:
-		bestand('', 'log', i)
-		print(i)
 
-print('open an file')
-userinput=os.path.join(input())
-logfile=userinput+'.log'
-bestand(userinput, 'openen','')
-input('press enter to continue')
+def main():
+	'pass data around, call nmap'
+	found=findIP()
+	logging(found)
+	nmap=subprocess.Popen(nmap_args)
+
+def logging(text):
+	with open(logfile, 'w') as log:
+		for i in text:
+			log.write(i)
+			log.write('\n')
+
+def findIP():
+	lijst=[]
+	reg= re.compile(r'\d+\.\d+\.\d+\.\d+(?<!Accepted)') #find all ipaddresses, except for lines saying 'accepted'
+	with open(uinput) as file:
+		results=reg.findall(file.read()) 
+		results.sort()
+		for i in results: #duplicate filter
+			if i not in lijst:
+				lijst+=[i]
+	return lijst
+
+if __name__=='__main__':
+	main()
