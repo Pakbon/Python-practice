837cc88181580eceb106790477e6b185fd2d2b17
Ecko <bboy_19ecko89@hotmail.com>
2018-11-20 13:58:12 +0100

    rewritten announcements.py

--- announcements.py
+++ announcements.py
@@ -1,57 +1,75 @@
+'''keeps track of rss feeds within a steamgroup and sends a mail on new 
+announcement'''
+
 import requests
+import shelve
+import json
 import feedparser
 import smtplib
-import json
+import re
 
 from bs4 import BeautifulSoup as bs
 from email.message import EmailMessage
 
-with open('announcements.json') as config:
-    config = json.loads(config.read())
+with open('files/announcements.json') as file:
+    config = json.loads(file.read())
+
 
 def main():
-    articledict = parsing()
-    mail(articledict)
+    announcement = parsing()
+    details(announcement)
+    return 0
 
-def parsing():
-    url = config['rsslinks'][0]
-    newsfeed = feedparser.parse(url)
-    soup = bs(newsfeed['entries'][2]['summary'], features='html.parser')
-    articledict = {}
-    articledict['articlelink'] = newsfeed.entries[0]['links'][0]['href']
-    articledict['title'] = newsfeed.entries[0]['title']
-    articledict['games'] = {}
-    articledict['bundlelink'] = soup.contents[0].text
 
-    test = soup.select('.bb_ul')
-    gamelinks = test[0].find_all('a', 'bb_link')
-    gamedict = []
-    for items in gamelinks:
-        game = items.contents[0]
-        link = items.get('href')
+def parsing(entry=0):
+    'parse rss feed. Defaults to first item returned. entry=0'
+    url = config['rsslink']
+    fetched = requests.get(url)
+    soup = bs(fetched.text, features='html.parser')
+    items = soup.find_all('item')
+    return items[entry]
+
+
+def details(announcement):
+    'checks whether title is in list in announcements.json and parse details'
+    for names in config['bundlename']:
+        regex = re.compile(names, re.IGNORECASE)
+        title = announcement.title.text
+        if regex.search(title):
+            if saving(title) == 0:
+                mail(announcement)
+            else:
+                # already existing
+                return
+        else:
+            continue
+
+
+def saving(title):
+    file = shelve.open('files/announcements.shv')
+    try:
+        if title in file['announcements']:
+            return 1
+    except KeyError:
         try:
-            items.contents[1]
-            stc = 1
-        except IndexError:
-            stc = 0
-        gamedict += [{'game' : game, 'link' : link, 'stc' : stc}]
-    articledict['games'] = gamedict
-    return articledict
+            file['announcements'] += [title]
+            return 0
+        except KeyError:
+            file['announcements'] = [title]
+            return 0
 
-def mail(articledict):
+
+def mail(announcement):
     msg = EmailMessage()
-    msg['Subject'] = articledict['title']
-    msg['from'] = config['from']
-    msg['to'] = config['to']
-    content = 'Bundlelink: {}\n'.format(articledict['bundlelink'])
-    content += 'Game, has cards?\n\n'
-    for game in articledict['games']:
-        content += '{},{} : {}\n'.format(game['stc'], game['game'], game['link'])
-    msg.set_content(content)
+    msg.set_content(announcement.guid.text)
+    msg['Subject'] = announcement.title.text
+    msg['From'] = config['from']
+    msg['To'] = config['to']
 
-    with smtplib.SMTP(config['smtpserver'], config['smtpport']) as smtpobj:
-        smtpobj.ehlo()
-        smtpobj.send_message(msg)
+    smtpconnect = smtplib.SMTP(
+        host=config['smtpserver'], port=config['smtpport'])
+    smtpconnect.send_message(msg)
+    smtpconnect.quit()
 
 
 if __name__ == '__main__':
